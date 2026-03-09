99724e38a13330a876ec90ad5a4f1ae033053f39
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-09 21:01:10 +0530

    Create salon service interface with crud methods

--- salon-service/src/main/java/com/meerthika/service/SalonService.java
+++ salon-service/src/main/java/com/meerthika/service/SalonService.java
@@ -4,6 +4,18 @@ import com.meerthika.modal.Salon;
 import com.meerthika.payload.dto.SalonDTO;
 import com.meerthika.payload.dto.UserDTO;
 
+import java.util.List;
+
 public interface SalonService {
     Salon createSalon(SalonDTO salon, UserDTO user);
+
+    Salon updateSalon(SalonDTO salon, UserDTO user, Long salonId);
+
+    List<Salon> getAllSalons();
+
+    Salon getSalonById(Long salonId);
+
+    Salon getSalonByOwnerId(Long ownerId);
+
+    List<Salon> searchSalonByCity(String city);
 }

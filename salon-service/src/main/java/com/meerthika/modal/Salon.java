0a8b331458d23f88483453ff8a834b023e1ebb52
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-09 20:49:37 +0530

    Create salon modal for salondb

---
+++ salon-service/src/main/java/com/meerthika/modal/Salon.java
@@ -0,0 +1,43 @@
+package com.meerthika.modal;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+public class Salon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection
+    private List<String> images;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private Long ownerId;
+
+    @Column(nullable = false)
+    private LocalTime openTime;
+
+    @Column(nullable = false)
+    private LocalTime closeTime;
+}

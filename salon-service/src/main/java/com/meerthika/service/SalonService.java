3c6f465d69c1aaa78776a5490d6aac905d12903e
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-09 20:57:03 +0530

    Import salon modal on salon service

--- salon-service/src/main/java/com/meerthika/service/SalonService.java
+++ salon-service/src/main/java/com/meerthika/service/SalonService.java
@@ -1,6 +1,8 @@
 package com.meerthika.service;
 
+import com.meerthika.modal.Salon;
 import com.meerthika.payload.dto.SalonDTO;
+import com.meerthika.payload.dto.UserDTO;
 
 public interface SalonService {
     Salon createSalon(SalonDTO salon, UserDTO user);

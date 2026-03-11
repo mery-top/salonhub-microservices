c35b433dc315cc56bb2b088655bf4e74d55da1d3
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-11 08:05:53 +0530

    Implement createSalon method

--- salon-service/src/main/java/com/meerthika/service/impl/SalonServiceImpl.java
+++ salon-service/src/main/java/com/meerthika/service/impl/SalonServiceImpl.java
@@ -3,15 +3,31 @@ package com.meerthika.service.impl;
 import com.meerthika.modal.Salon;
 import com.meerthika.payload.dto.SalonDTO;
 import com.meerthika.payload.dto.UserDTO;
+import com.meerthika.repository.SalonRepository;
 import com.meerthika.service.SalonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SalonServiceImpl implements SalonService {
+
+
+    public final SalonRepository salonRepository;
+
     @Override
-    public Salon createSalon(SalonDTO salon, UserDTO user) {
+    public Salon createSalon(SalonDTO req, UserDTO user) {
+
+        Salon salon = new Salon();
+        salon.setName(req.getName());
+        salon.setAddress(req.getAddress());
+        salon.setEmail(req.getEmail());
+        salon.setCity(req.getCity());
+        salon.setImages(req.getImages());
+        salon.setOwnerId(user.getId());
+
         return null;
     }
 

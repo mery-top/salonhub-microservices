77d062f0b3709c788eac44a7d34d2c1539939ed5
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-11 09:11:19 +0530

    Add createsalon controller

--- salon-service/src/main/java/com/meerthika/controller/SalonController.java
+++ salon-service/src/main/java/com/meerthika/controller/SalonController.java
@@ -1,7 +1,14 @@
 package com.meerthika.controller;
 
+import com.meerthika.mapper.SalonMapper;
+import com.meerthika.modal.Salon;
+import com.meerthika.payload.dto.SalonDTO;
+import com.meerthika.payload.dto.UserDTO;
 import com.meerthika.service.SalonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,15 @@ public class SalonController {
 
     private final SalonService salonService;
 
+    @PostMapping
+    public ResponseEntity<SalonDTO> createSalon(@RequestBody SalonDTO salonDTO){
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        Salon salon = salonService.createSalon(salonDTO, userDTO);
+
+        SalonDTO salonDTO1 = SalonMapper.mapToDTO(salon);
+        return ResponseEntity.ok(salonDTO1);
+    }
 
 }

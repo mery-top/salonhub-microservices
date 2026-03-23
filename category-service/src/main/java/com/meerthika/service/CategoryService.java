0b021404fc0c2f971847842734f5b87ebe916e28
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-23 15:43:45 +0530

    Add exception method signature to interface

--- category-service/src/main/java/com/meerthika/service/CategoryService.java
+++ category-service/src/main/java/com/meerthika/service/CategoryService.java
@@ -9,6 +9,6 @@ public interface CategoryService {
 
     Category saveCategory(Category category, SalonDTO salonDTO);
     Set<Category> getAllCategoriesBySalon(Long id);
-    Category getCategoryById(Long id);
-    void deleteCategoryById(Long id);
+    Category getCategoryById(Long id) throws Exception;
+    void deleteCategoryById(Long id) throws Exception;
 }

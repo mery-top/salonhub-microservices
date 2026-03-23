5d3adfd3e8051a843248c29a75c0bc6bf4c4c89b
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-23 15:29:48 +0530

    Add service annotation to category service

---
+++ category-service/src/main/java/com/meerthika/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,38 @@
+package com.meerthika.service.impl;
+
+import com.meerthika.dto.SalonDTO;
+import com.meerthika.modal.Category;
+import com.meerthika.repository.CategoryRepository;
+import com.meerthika.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+
+
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public Category saveCategory(Category category, SalonDTO salonDTO) {
+        return null;
+    }
+
+    @Override
+    public Set<Category> getAllCategoriesBySalon(Long id) {
+        return Set.of();
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+
+    }
+}

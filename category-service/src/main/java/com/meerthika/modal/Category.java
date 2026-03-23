997400c59acddc12b66e6cd281e9490e3d8c80db
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-23 15:23:26 +0530

    Implement the category modal

---
+++ category-service/src/main/java/com/meerthika/modal/Category.java
@@ -0,0 +1,22 @@
+package com.meerthika.modal;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String image;
+
+    @Column(nullable = false)
+    private Long salonId;
+}

6b9c139ab74992c3908afb00b30e12a910eeaced
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-23 16:08:48 +0530

    Move create and delete category under owner endpoint

---
+++ category-service/src/main/java/com/meerthika/controller/SalonCategoryController.java
@@ -0,0 +1,40 @@
+package com.meerthika.controller;
+
+import com.meerthika.dto.SalonDTO;
+import com.meerthika.modal.Category;
+import com.meerthika.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/categories/salon-owner")
+public class SalonCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        SalonDTO salonDTO = new SalonDTO();
+        salonDTO.setId(1L);
+        Category savedCategory = categoryService.saveCategory(category, salonDTO);
+
+        return ResponseEntity.ok(savedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCategory(@PathVariable Long id) throws Exception{
+        SalonDTO salonDTO = new SalonDTO();
+        salonDTO.setId(1L);
+        categoryService.deleteCategoryById(id, salonDTO.getId());
+
+
+        return ResponseEntity.ok("category deleted sucesssfully");
+    }
+
+
+}

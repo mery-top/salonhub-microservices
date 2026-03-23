40e17d40c9f30f52b249594f2698e0b419176b53
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-23 14:50:33 +0530

    Add salon id to the salon dto

--- salon-service/src/main/java/com/meerthika/mapper/SalonMapper.java
+++ salon-service/src/main/java/com/meerthika/mapper/SalonMapper.java
@@ -8,7 +8,7 @@ public class SalonMapper {
     public static SalonDTO mapToDTO(Salon salon){
 
         SalonDTO salonDTO = new SalonDTO();
-
+        salonDTO.setId(salon.getId());
         salonDTO.setName(salon.getName());
         salonDTO.setAddress(salon.getAddress());
         salonDTO.setCity(salon.getCity());
@@ -17,6 +17,7 @@ public class SalonMapper {
         salonDTO.setOpenTime(salon.getOpenTime());
         salonDTO.setPhoneNumber(salon.getPhoneNumber());
         salonDTO.setOwnerId(salon.getOwnerId());
+        salonDTO.setEmail(salon.getEmail());
 
         return salonDTO;
     }

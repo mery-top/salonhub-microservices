4daca64da6c157a154b51749175e9f297be1baa2
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 15:51:28 +0530

    fix(service): include exception handling

--- service-offering/src/main/java/com/meerthika/service/ServiceOfferingService.java
+++ service-offering/src/main/java/com/meerthika/service/ServiceOfferingService.java
@@ -12,7 +12,7 @@ import java.util.Set;
 public interface ServiceOfferingService {
 
     ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO);
-    ServiceOffering updateService(Long serviceId, ServiceOffering service);
+    ServiceOffering updateService(Long serviceId, ServiceOffering service) throws Exception;
     Set<ServiceOffering> getAllServicesBySalonId(Long salonId, Long categoryId);
     Set<ServiceOffering> getServicesByIds(Set<Long> ids);
 

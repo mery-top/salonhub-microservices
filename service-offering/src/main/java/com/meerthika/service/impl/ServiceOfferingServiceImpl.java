52d7a63af151350bf66f941d8182deda322386e0
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 15:58:23 +0530

    feat(impl): Add getAllServicesby salon id implementation

--- service-offering/src/main/java/com/meerthika/service/impl/ServiceOfferingServiceImpl.java
+++ service-offering/src/main/java/com/meerthika/service/impl/ServiceOfferingServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -54,7 +55,15 @@ public class ServiceOfferingServiceImpl implements ServiceOfferingService {
 
     @Override
     public Set<ServiceOffering> getAllServicesBySalonId(Long salonId, Long categoryId) {
-        return Set.of();
+
+        Set<ServiceOffering> services = serviceOfferingRepository.findBySalonId(salonId);
+
+        if(categoryId!=null){
+            services = services.stream().filter((service) -> service.getCategoryId() != null &&
+                    service.getCategoryId() == categoryId).collect(Collectors.toSet());
+        }
+
+        return services;
     }
 
     @Override

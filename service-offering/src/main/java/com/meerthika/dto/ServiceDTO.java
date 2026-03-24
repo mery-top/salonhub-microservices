d859342ecd494954d9f950769630dca041923a9c
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 14:32:01 +0530

    refactor(servicedto): remove sql annotations

---
+++ service-offering/src/main/java/com/meerthika/dto/ServiceDTO.java
@@ -0,0 +1,30 @@
+package com.meerthika.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class ServiceDTO {
+
+    private Long id;
+
+    private String name;
+
+
+    private String description;
+
+
+    private int price;
+
+
+    private int duration;
+
+
+    private Long salonId;
+
+
+    private Long categoryId;
+
+    private String image;
+}

ce045e64c4cfba2e20451016739d41972a063507
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 14:18:28 +0530

    build: ServiceOffering modal

---
+++ service-offering/src/main/java/com/meerthika/modal/ServiceOffering.java
@@ -0,0 +1,21 @@
+package com.meerthika.modal;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class ServiceOffering {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+}

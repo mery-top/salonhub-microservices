724cbae4e865d5cea9c37156a4695c90d33c3164
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 14:41:58 +0530

    feat(repo): Add service offering repository

---
+++ service-offering/src/main/java/com/meerthika/repository/ServiceOfferingRepository.java
@@ -0,0 +1,13 @@
+package com.meerthika.repository;
+
+import com.meerthika.modal.ServiceOffering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface ServiceOfferingRepository extends JpaRepository<ServiceOffering, Long> {
+
+    Set<ServiceOffering> findBySalonId(Long salonId);
+
+
+}

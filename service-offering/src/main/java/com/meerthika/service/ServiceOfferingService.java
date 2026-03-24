8524d1d8f232d913c771d3423c0034813af435fb
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-24 14:36:54 +0530

    refactor(service): add update, getall, get all by id for service offering

--- service-offering/src/main/java/com/meerthika/service/ServiceOfferingService.java
+++ service-offering/src/main/java/com/meerthika/service/ServiceOfferingService.java
@@ -7,7 +7,14 @@ import com.meerthika.dto.ServiceDTO;
 import com.meerthika.modal.ServiceOffering;
 import jdk.jfr.Category;
 
+import java.util.Set;
+
 public interface ServiceOfferingService {
 
     ServiceOffering createService(SalonDTO salonDTO, ServiceDTO serviceDTO, CategoryDTO categoryDTO);
+    ServiceOffering updateService(Long serviceId, ServiceOffering service);
+    Set<ServiceOffering> getAllServicesBySalonId(Long salonId, Long categoryId);
+    Set<ServiceOffering> getServicesByIds(Set<Long> ids);
+
+
 }

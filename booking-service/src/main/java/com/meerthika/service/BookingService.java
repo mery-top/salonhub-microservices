c85af201e8f0c0b8762f801482f7527433319948
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-25 12:03:34 +0530

    feat(service): create booking service interface

--- booking-service/src/main/java/com/meerthika/service/BookingService.java
+++ booking-service/src/main/java/com/meerthika/service/BookingService.java
@@ -1,10 +1,14 @@
 package com.meerthika.service;
 
+import com.meerthika.domain.BookingStatus;
+import com.meerthika.dto.BookingRequest;
 import com.meerthika.dto.SalonDTO;
 import com.meerthika.dto.ServiceDTO;
 import com.meerthika.dto.UserDTO;
 import com.meerthika.modal.Booking;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
 public interface BookingService {
@@ -14,4 +18,11 @@ public interface BookingService {
                           UserDTO user, SalonDTO salon,
                           Set<ServiceDTO> serviceDTOSet
                           );
+
+    List<Booking> getBookingsByCustomer(Long customerId);
+    List<Booking> getBookingsBySalon(Long salonId);
+
+    Booking getBookingById(Long id);
+    Booking updateBooking(Long bookingId, BookingStatus status);
+    List<Booking> getBookingsByDate(LocalDate date, Long salonId);
 }

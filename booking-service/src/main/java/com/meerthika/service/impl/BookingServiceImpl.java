f57be21544a09be677553e063a5c1e428d80499f
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-25 13:44:21 +0530

    feat(impl): add createbooking function

--- booking-service/src/main/java/com/meerthika/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/meerthika/service/impl/BookingServiceImpl.java
@@ -13,8 +13,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -25,8 +27,63 @@ public class BookingServiceImpl implements BookingService {
 
 
     @Override
-    public Booking createBooking(BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet) {
-        return null;
+    public Booking createBooking(BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet) throws Exception {
+
+        int totalDuration = serviceDTOSet.stream()
+                .mapToInt(ServiceDTO::getDuration)
+                .sum();
+
+        LocalDateTime bookingStartTime = booking.getStartTime();
+        LocalDateTime bookingEndTime= bookingStartTime.plusMinutes(totalDuration);
+
+        Boolean isSlotAvailable = isTimeSlotAvailable(salon, bookingStartTime, bookingEndTime);
+
+        int totalPrice = serviceDTOSet.stream()
+                .mapToInt(ServiceDTO::getPrice)
+                .sum();
+
+        Set<Long> idList = serviceDTOSet.stream()
+                .map(ServiceDTO:: getId)
+                .collect(Collectors.toSet());
+
+        Booking newBooking = new Booking();
+
+        newBooking.setCustomerId(user.getId());
+        newBooking.setSalonId(salon.getId());
+        newBooking.setServiceIds(idList);
+        newBooking.setStatus(BookingStatus.PENDING);
+        newBooking.setStartTime(bookingStartTime);
+        newBooking.setEndTime(bookingEndTime);
+        newBooking.setTotalPrice(totalPrice);
+
+        return bookingRepository.save(newBooking);
+    }
+
+    public Boolean isTimeSlotAvailable(SalonDTO salonDTO, LocalDateTime bookingStartTime, LocalDateTime bookingEndTime) throws Exception {
+
+        List<Booking> existingBookings = getBookingsBySalon(salonDTO.getId());
+        LocalDateTime salonOpenTime = salonDTO.getOpenTime().atDate(bookingStartTime.toLocalDate());
+        LocalDateTime salonCloseTime = salonDTO.getCloseTime().atDate(bookingStartTime.toLocalDate());
+
+        if(bookingStartTime.isBefore(salonOpenTime) || bookingEndTime.isAfter(salonCloseTime)){
+            throw new Exception("Booking time must be within salon's working hours");
+        }
+
+        for(Booking existingBooking: existingBookings){
+            LocalDateTime existingBookingStartTime = existingBooking.getStartTime();
+            LocalDateTime existingBookingEndTime = existingBooking.getEndTime();
+
+            if(bookingStartTime.isBefore(existingBookingEndTime) && bookingEndTime.isAfter(existingBookingStartTime)){
+                throw new Exception("slot not available, choose different time");
+            }
+
+            if(bookingStartTime.isEqual(existingBookingStartTime) || bookingEndTime.isEqual(existingBookingEndTime)){
+                throw new Exception("slot not available, choose different time");
+            }
+
+        }
+
+        return true;
     }
 
     @Override

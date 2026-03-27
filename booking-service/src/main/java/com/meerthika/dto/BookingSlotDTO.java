34f728ac7f71bb98478f7758546632ae8b80b191
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 10:01:51 +0530

    feat(dto): create BookingSlotDTO

---
+++ booking-service/src/main/java/com/meerthika/dto/BookingSlotDTO.java
@@ -0,0 +1,12 @@
+package com.meerthika.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+public class BookingSlotDTO {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}

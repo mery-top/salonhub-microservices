31dc43f673dc801c56ab2362b963d1ced465b684
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 09:37:25 +0530

    feat(mapper): create booking mapper

---
+++ booking-service/src/main/java/com/meerthika/mapper/BookingMapper.java
@@ -0,0 +1,21 @@
+package com.meerthika.mapper;
+
+import com.meerthika.dto.BookingDTO;
+import com.meerthika.modal.Booking;
+
+public class BookingMapper {
+
+
+    public static BookingDTO toDTO(Booking booking){
+        BookingDTO bookingDTO = new BookingDTO();
+        bookingDTO.setId(booking.getId());
+        bookingDTO.setCustomerId(booking.getCustomerId());
+        bookingDTO.setStatus(booking.getStatus());
+        bookingDTO.setEndTime(booking.getEndTime());
+        bookingDTO.setStartTime(booking.getStartTime());
+        bookingDTO.setSalonId(booking.getSalonId());
+        bookingDTO.setServiceIds(booking.getServiceIds());
+
+        return bookingDTO;
+    }
+}

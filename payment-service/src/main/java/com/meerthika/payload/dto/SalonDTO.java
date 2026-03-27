e61601210d45b26d0d3fac468d870a8eea3e0eed
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 11:42:23 +0530

    refactor(payment-dto): import dtos from booking microservice

---
+++ payment-service/src/main/java/com/meerthika/payload/dto/SalonDTO.java
@@ -0,0 +1,35 @@
+package com.meerthika.payload.dto;
+
+import com.meerthika.dto.UserDTO;
+import lombok.Data;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+public class SalonDTO {
+
+
+    private Long id;
+
+    private String name;
+
+    private List<String> images;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String email;
+
+    private String city;
+
+    private Long ownerId;
+
+    //we add this user details that's why dto is required
+    private UserDTO owner;
+
+    private LocalTime openTime;
+
+    private LocalTime closeTime;
+}

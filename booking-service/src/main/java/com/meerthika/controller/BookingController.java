fc698b9e85ac7df82568e8a7955f0735832261df
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 09:23:39 +0530

    feat(controller): Add create booking for booking controller

---
+++ booking-service/src/main/java/com/meerthika/controller/BookingController.java
@@ -0,0 +1,47 @@
+package com.meerthika.controller;
+
+
+import com.meerthika.dto.BookingRequest;
+import com.meerthika.dto.SalonDTO;
+import com.meerthika.dto.ServiceDTO;
+import com.meerthika.dto.UserDTO;
+import com.meerthika.modal.Booking;
+import com.meerthika.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(@RequestParam Long salonId, @RequestBody BookingRequest bookingRequest) throws Exception {
+        UserDTO user = new UserDTO();
+        user.setId(1L);
+
+        SalonDTO salon = new SalonDTO();
+        salon.setId(salonId);
+
+        Set<ServiceDTO> serviceDTOSet = new HashSet<>();
+        ServiceDTO serviceDTO = new ServiceDTO();
+
+        serviceDTO.setId(1L);
+        serviceDTO.setPrice(399);
+        serviceDTO.setDuration(45);
+        serviceDTO.setName("Hair cut for men");
+
+        serviceDTOSet.add(serviceDTO);
+
+        Booking booking = bookingService.createBooking(bookingRequest, user, salon, serviceDTOSet);
+
+        return ResponseEntity.ok(booking);
+
+    }
+}

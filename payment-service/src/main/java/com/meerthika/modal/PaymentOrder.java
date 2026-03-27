f89b08fa9c0edd4584be4829db5c349d681bebdf
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 11:24:16 +0530

    feat(payment-modal): Add payment Order modal

--- payment-service/src/main/java/com/meerthika/modal/PaymentOrder.java
+++ payment-service/src/main/java/com/meerthika/modal/PaymentOrder.java
@@ -1,17 +1,37 @@
 package com.meerthika.modal;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.meerthika.domain.PaymentMethod;
+import com.meerthika.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class PaymentOrder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false)
     private Long amount;
+
+    @Column(nullable = false)
+    private PaymentOrderStatus status=PaymentOrderStatus.PENDING;
+
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinkId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long bookingId;
+
+    @Column(nullable = false)
+    private Long salonId;
+
 }

89b050d984167cda1341584cbfd7a7b9c6e8ea10
Meerthika <meerthikasrrajendrans@gmail.com>
2026-03-27 09:37:13 +0530

    feat(dto): create booking dto

---
+++ booking-service/src/main/java/com/meerthika/dto/BookingDTO.java
@@ -0,0 +1,30 @@
+package com.meerthika.dto;
+
+import com.meerthika.domain.BookingStatus;
+import jakarta.persistence.ElementCollection;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Data
+public class BookingDTO {
+
+
+    private Long Id;
+
+    private Long salonId;
+
+    private Long customerId;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private Set<Long> serviceIds;
+
+    private BookingStatus status = BookingStatus.PENDING;
+
+    private int totalPrice;
+}

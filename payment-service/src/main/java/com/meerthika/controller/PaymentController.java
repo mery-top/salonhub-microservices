ad4ca99f107009f1c654557dda05091c7855ec32
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-06 09:32:32 +0530

    feat(payment-controller): Implement proceedPayment method

--- payment-service/src/main/java/com/meerthika/controller/PaymentController.java
+++ payment-service/src/main/java/com/meerthika/controller/PaymentController.java
@@ -45,6 +45,18 @@ public class PaymentController {
         return ResponseEntity.ok(res);
     }
 
+    @PatchMapping("/proceed")
+    public ResponseEntity<Boolean> proceedPayment(
+            @RequestParam String paymentId,
+            @RequestParam String paymentLinkId
+    ) throws Exception {
+
+        PaymentOrder paymentOrder = paymentService.getPaymentOrderByPaymentId(paymentLinkId);
+        Boolean res = paymentService.proceedPayment(paymentOrder, paymentId, paymentLinkId);
+        return ResponseEntity.ok(res);
+
+    }
+
 
 
 }

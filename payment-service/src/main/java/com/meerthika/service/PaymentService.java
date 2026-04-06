cceeba824ecdf79bd853c9705d93569a5c81c33b
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-06 07:54:49 +0530

    refactor(payment-impl): Add throws Exception

--- payment-service/src/main/java/com/meerthika/service/PaymentService.java
+++ payment-service/src/main/java/com/meerthika/service/PaymentService.java
@@ -7,17 +7,18 @@ import com.meerthika.payload.dto.UserDTO;
 import com.meerthika.payload.response.PaymentLinkResponse;
 import com.razorpay.PaymentLink;
 import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
 
 public interface PaymentService {
 
     PaymentLinkResponse createOrder(UserDTO user,
                                     BookingDTO booking,
                                     PaymentMethod paymentMethod
-    );
+    ) throws StripeException, RazorpayException;
 
     PaymentOrder getPaymentOrderById(Long id) throws Exception;
     PaymentOrder getPaymentOrderByPaymentId(String paymentId);
 
     PaymentLink createRazorpayPaymentLink(UserDTO user, Long amount, Long orderId) throws RazorpayException;
-    String createStripePaymentLink(UserDTO user, Long amount, Long orderId);
+    String createStripePaymentLink(UserDTO user, Long amount, Long orderId) throws StripeException;
 }

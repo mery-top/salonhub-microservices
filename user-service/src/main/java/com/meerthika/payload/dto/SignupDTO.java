2c1feee6699ded0bef6b43c856bb3e86615bb890
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-12 20:48:06 +0530

    feat(user-dto): Add signup dto for user service

---
+++ user-service/src/main/java/com/meerthika/payload/dto/SignupDTO.java
@@ -0,0 +1,14 @@
+package com.meerthika.payload.dto;
+
+
+import lombok.Data;
+
+@Data
+public class SignupDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String username;
+
+}

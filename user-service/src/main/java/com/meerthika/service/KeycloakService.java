039ba5ea9f1637aba5ee89bd2a8ab258be0fd72f
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-12 20:35:11 +0530

    feat(user-keycloak): Add keycloak service

---
+++ user-service/src/main/java/com/meerthika/service/KeycloakService.java
@@ -0,0 +1,37 @@
+package com.meerthika.service;
+
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class KeycloakService {
+    static Dotenv dotenv = Dotenv.load();
+    private static final String KeycloakClientSecret = dotenv.get("KEYCLOAK_CLIENT_SECRET");
+    private static final String KeycloakClientId = dotenv.get("KEYCLOAK_CLIENT_ID");
+    public void test(){
+        System.out.println(KeycloakClientSecret);
+    }
+
+    private static final String KEYCLOAK_BASE_URL="http://localhost:8080";
+    private static final String KEYCLOAK_ADMIN_API= KEYCLOAK_BASE_URL+"/admin/realms/master/users";
+    private static final String TOKEN_URL=KEYCLOAK_BASE_URL+"/realms/master/protocol/openid-connect/token";
+    private static final String CLIENT_ID=KeycloakClientId;
+    private static final String CLIENT_SECRET=KeycloakClientSecret;
+
+
+    private static final String GRANT_TYPE="password";
+    private static final String scope = "openid profile email";
+    private static final String username="admin";
+    private static final String password= "admin";
+    private static final String clientId="5de187fe-b51b-4c44-8454-a61912c5a087";
+
+
+
+
+
+
+
+}

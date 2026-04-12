47a4b628636886c9a96e6a7e52f48262c8fa5444
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-12 20:47:58 +0530

    feat(user-dto): Add credential dto for user service

---
+++ user-service/src/main/java/com/meerthika/payload/dto/Credential.java
@@ -0,0 +1,11 @@
+package com.meerthika.payload.dto;
+
+
+import lombok.Data;
+
+@Data
+public class Credential {
+    private String type;
+    private String value;
+    private String temporary;
+}

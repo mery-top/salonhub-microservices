3410adc288e52ac5c5311654a529693eaa2c216f
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-13 14:48:52 +0530

    refactor(user-controller): To send email instead of username

--- user-service/src/main/java/com/meerthika/controller/AuthController.java
+++ user-service/src/main/java/com/meerthika/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
     public ResponseEntity<AuthResponse> login(
             @RequestBody LoginDTO req
     ) throws Exception {
-        AuthResponse res = authService.login(req.getUsername(), req.getPassword());
+        AuthResponse res = authService.login(req.getEmail(), req.getPassword());
         return ResponseEntity.ok(res);
     }
 

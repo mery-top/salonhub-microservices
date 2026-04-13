73e24918aea1e443f5f1de1c3fd44092b3c338a7
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-13 14:28:57 +0530

    feat(user-controller): Add auth controller for auth service

---
+++ user-service/src/main/java/com/meerthika/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.meerthika.controller;
+
+import com.meerthika.payload.dto.LoginDTO;
+import com.meerthika.payload.dto.SignupDTO;
+import com.meerthika.payload.response.AuthResponse;
+import com.meerthika.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> signup(
+            @RequestBody SignupDTO req
+            ) throws Exception {
+        AuthResponse res = authService.signup(req);
+        return ResponseEntity.ok(res);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(
+            @RequestBody LoginDTO req
+    ) throws Exception {
+        AuthResponse res = authService.login(req.getUsername(), req.getPassword());
+        return ResponseEntity.ok(res);
+    }
+
+    @GetMapping("/access-token/refresh-token/{refreshToken}")
+    public ResponseEntity<AuthResponse> getAccessToken(
+            @PathVariable String refreshToken
+    ) throws Exception {
+        AuthResponse res = authService.getAccessTokenFromRefreshToken(refreshToken);
+        return ResponseEntity.ok(res);
+    }
+
+}

667920886ac317e35d65cd08f0515a1168ac171b
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-13 12:42:44 +0530

    feat(user-impl): Add signup implementation for auth service

--- user-service/src/main/java/com/meerthika/service/impl/AuthServiceImpl.java
+++ user-service/src/main/java/com/meerthika/service/impl/AuthServiceImpl.java
@@ -1,12 +1,16 @@
 package com.meerthika.service.impl;
 
+import com.meerthika.modal.User;
 import com.meerthika.payload.dto.SignupDTO;
 import com.meerthika.payload.response.AuthResponse;
+import com.meerthika.payload.response.TokenResponse;
 import com.meerthika.respository.UserRepository;
 import com.meerthika.service.AuthService;
 import com.meerthika.service.KeycloakService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +26,32 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
-    public AuthResponse signup(SignupDTO req) {
-        return null;
+    public AuthResponse signup(SignupDTO req) throws Exception {
+        keycloakService.createUser(req);
+
+        User user = new User();
+        user.setUsername(req.getUsername());
+        user.setPassword(req.getPassword());
+        user.setEmail(req.getEmail());
+        user.setRole(req.getRole());
+        user.setFullName(req.getFirstName());
+        user.setCreatedAt(LocalDateTime.now());
+
+        userRepository.save(user);
+
+        TokenResponse tokenResponse = keycloakService.getAdminAccessToken(
+                req.getUsername(),
+                req.getPassword(),
+                "password", null
+        );
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setRefresh_token(tokenResponse.getRefreshToken());
+        authResponse.setJwt(tokenResponse.getAccessToken());
+        authResponse.setRole(user.getRole());
+        authResponse.setMessage("Register success");
+
+        return authResponse;
     }
 
     @Override

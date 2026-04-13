13b532c62f68f17d99c85bdf1b9ffb8db8814a7b
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-13 09:57:45 +0530

    feat(user-dto): Add keycloakrole dto

---
+++ user-service/src/main/java/com/meerthika/payload/dto/KeycloakRole.java
@@ -0,0 +1,15 @@
+package com.meerthika.payload.dto;
+
+import java.util.Map;
+
+public class KeycloakRole {
+
+    private String id;
+    private String name;
+    private String description;
+    private boolean composite;
+    private boolean clientRole;
+    private String containerId;
+    private Map<String, Object> attributes;
+
+}

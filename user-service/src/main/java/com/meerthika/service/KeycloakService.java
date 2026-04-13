27e343f1f02226e4cde67e2f52b0b3b1dd243046
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-13 10:39:37 +0530

    feat(user-service): Add create user function

--- user-service/src/main/java/com/meerthika/service/KeycloakService.java
+++ user-service/src/main/java/com/meerthika/service/KeycloakService.java
@@ -1,14 +1,20 @@
 package com.meerthika.service;
 
 
-import com.meerthika.payload.dto.Credential;
-import com.meerthika.payload.dto.SignupDTO;
-import com.meerthika.payload.dto.UserRequest;
+import com.meerthika.payload.dto.*;
+import com.meerthika.payload.response.TokenResponse;
 import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.MultivaluedHashMap;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Data
@@ -17,28 +23,30 @@ public class KeycloakService {
     static Dotenv dotenv = Dotenv.load();
     private static final String KeycloakClientSecret = dotenv.get("KEYCLOAK_CLIENT_SECRET");
     private static final String KeycloakClientId = dotenv.get("KEYCLOAK_CLIENT_ID");
-    public void test(){
+
+    public void test() {
         System.out.println(KeycloakClientSecret);
     }
 
-    private static final String KEYCLOAK_BASE_URL="http://localhost:8080";
-    private static final String KEYCLOAK_ADMIN_API= KEYCLOAK_BASE_URL+"/admin/realms/master/users";
-    private static final String TOKEN_URL=KEYCLOAK_BASE_URL+"/realms/master/protocol/openid-connect/token";
-    private static final String CLIENT_ID=KeycloakClientId;
-    private static final String CLIENT_SECRET=KeycloakClientSecret;
+    private static final String KEYCLOAK_BASE_URL = "http://localhost:8080";
+    private static final String KEYCLOAK_ADMIN_API = KEYCLOAK_BASE_URL + "/admin/realms/master/users";
+    private static final String TOKEN_URL = KEYCLOAK_BASE_URL + "/realms/master/protocol/openid-connect/token";
+    private static final String CLIENT_ID = KeycloakClientId;
+    private static final String CLIENT_SECRET = KeycloakClientSecret;
 
 
-    private static final String GRANT_TYPE="password";
+    private static final String GRANT_TYPE = "password";
     private static final String scope = "openid profile email";
-    private static final String username="admin";
-    private static final String password= "admin";
-    private static final String clientId="5de187fe-b51b-4c44-8454-a61912c5a087";
+    private static final String username = "zosh";
+    private static final String password = "admin";
+    //put the correct client id
+    private static final String clientId = "bcd0935c-93b8-4ac3-bc7e-c96361575415";
 
     private final RestTemplate restTemplate;
 
-    public void createUser(SignupDTO signupDTO) throws Exception{
+    public void createUser(SignupDTO signupDTO) throws Exception {
 
-        String ACCESS_TOKEN="";
+        String ACCESS_TOKEN = getAdminAccessToken(username, password, GRANT_TYPE, null).getAccessToken();
         Credential credential = new Credential();
         credential.setTemporary(false);
         credential.setType("password");
@@ -52,7 +60,7 @@ public class KeycloakService {
         userRequest.setLastName(signupDTO.getLastName());
         userRequest.setFirstName(signupDTO.getFirstName());
 
-        HttpHeaders headers= new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(ACCESS_TOKEN);
 
@@ -65,17 +73,79 @@ public class KeycloakService {
                 String.class
         );
 
-        if(response.getStatusCode()==HttpStatus.CREATED){
+        if (response.getStatusCode() == HttpStatus.CREATED) {
             System.out.println("user created successfully");
-        }
 
+            KeycloakUserDTO user = fetchFirstUserByUsername(signupDTO.getUsername(), ACCESS_TOKEN);
+            KeycloakRole role = getRoleByName(clientId, ACCESS_TOKEN, signupDTO.getRole().toString());
+
+            List<KeycloakRole> roles = new ArrayList<>();
+            roles.add(role);
+
+            assignRoleToUser(
+                user.getId(),
+                    clientId,
+                    roles,
+                    ACCESS_TOKEN
+            );
+        }else{
+            System.out.println("user creation failed");
+            throw new Exception(response.getBody());
+        }
+    }
+
+    public TokenResponse getAdminAccessToken(String username,
+                                             String password,
+                                             String grantType,
+                                             String refreshToken
+    ) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+
+        //one map with many values one key
+        MultivaluedMap<String, String> requestBody = new MultivaluedHashMap<>();
+        requestBody.add("grant_type", GRANT_TYPE);
+        requestBody.add("username", username);
+        requestBody.add("password", password);
+        requestBody.add("refresh_token", refreshToken);
+        requestBody.add("client_id", CLIENT_ID);
+        requestBody.add("client_secret", CLIENT_SECRET);
+
+        HttpEntity<MultivaluedMap<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                KEYCLOAK_ADMIN_API,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+
+        return null;
+    }
+
+    public KeycloakRole getRoleByName(
+            String clientId, String token, String role
+    ) {
+        return null;
     }
 
 
+    public KeycloakUserDTO fetchFirstUserByUsername(String username, String token) {
+        return null;
+    }
 
+    public void assignRoleToUser(
+            String userId,
+            String clientId,
+            List<KeycloakRole> roles,
+            String token
+    ){
 
-
-
-
-
+    }
 }
+
+
+

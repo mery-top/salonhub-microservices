5a8d3b63b122d32f3627a9deaeab87c3ac52dc0e
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-14 15:44:19 +0530

    feat(category-service): Add find by id and salon id method

--- category-service/src/main/java/com/meerthika/service/CategoryService.java
+++ category-service/src/main/java/com/meerthika/service/CategoryService.java
@@ -11,4 +11,5 @@ public interface CategoryService {
     Set<Category> getAllCategoriesBySalon(Long id);
     Category getCategoryById(Long id) throws Exception;
     void deleteCategoryById(Long id, Long salonId) throws Exception;
+    Category findByIdAndSalonId(Long id, Long SalonId) throws Exception;
 }

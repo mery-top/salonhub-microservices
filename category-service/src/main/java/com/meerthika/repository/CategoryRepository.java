560524396a0aaaa0ce3797f41c647758d3282c31
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-14 15:44:13 +0530

    feat(category-repo): Add find by id and salon id method

--- category-service/src/main/java/com/meerthika/repository/CategoryRepository.java
+++ category-service/src/main/java/com/meerthika/repository/CategoryRepository.java
@@ -9,6 +9,6 @@ import java.util.Set;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Set<Category> findBySalonId(Long SalonId);
-
+    Category findByIdAndSalonId(Long id, Long SalonId);
 
 }

2f29ea2e772b27045b62eaca86051acce55a5860
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-17 13:19:45 +0530

    feat(category-feign): Add category feign client

---
+++ category-service/src/main/java/com/meerthika/service/client/CategoryFeignClient.java
@@ -0,0 +1,19 @@
+package com.meerthika.service.client;
+
+
+import com.meerthika.dto.CategoryDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("CATEGORY-SERVICE")
+public interface CategoryFeignClient {
+
+    @GetMapping("/api/categories/{id}")
+    public ResponseEntity<CategoryDTO> getCategoryById(@PathVariable Long id)
+            throws Exception;
+    @GetMapping("/api/categories/salon-owner/salon/{salonId}/category/{id}")
+    public ResponseEntity<CategoryDTO> getCategoriesByIdAndSalon(@PathVariable Long id,
+                                                              @PathVariable Long salonId) throws Exception;
+}

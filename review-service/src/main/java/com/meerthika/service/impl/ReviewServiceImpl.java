6c7284a1068cf6e6c4b292ecfff49db77dae5be3
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-21 12:10:22 +0530

    feat(review-impl): Add create review implementation

--- review-service/src/main/java/com/meerthika/service/impl/ReviewServiceImpl.java
+++ review-service/src/main/java/com/meerthika/service/impl/ReviewServiceImpl.java
@@ -4,14 +4,25 @@ import com.meerthika.dto.ReviewRequest;
 import com.meerthika.dto.SalonDTO;
 import com.meerthika.dto.UserDTO;
 import com.meerthika.modal.Review;
+import com.meerthika.repository.ReviewRepository;
 import com.meerthika.service.ReviewService;
 
 import java.util.List;
 
 public class ReviewServiceImpl implements ReviewService {
+
+    private ReviewRepository reviewRepository;
+
     @Override
     public Review createReview(ReviewRequest req, UserDTO user, SalonDTO salon) {
-        return null;
+
+        Review review = new Review();
+        review.setReviewText(req.getReviewText());
+        review.setRating(req.getRating());
+        review.setUserId(user.getId());
+        review.setSalonId(salon.getId());
+
+        return reviewRepository.save(review);
     }
 
     @Override

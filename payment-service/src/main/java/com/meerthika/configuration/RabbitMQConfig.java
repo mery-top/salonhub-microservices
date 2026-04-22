d1f0908ea30c33bddd4802f8fd0d9cee3a0b2f87
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-22 13:10:14 +0530

    feat(payment-rabbit): Add rabbit mq template

---
+++ payment-service/src/main/java/com/meerthika/configuration/RabbitMQConfig.java
@@ -0,0 +1,27 @@
+package com.meerthika.configuration;
+
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+    public RabbitTemplate rabbitTemplate(
+            ConnectionFactory connectionFactory,
+            Jackson2JsonMessageConverter jackson2JsonMessageConverter
+    ){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
+        return rabbitTemplate;
+    }
+}

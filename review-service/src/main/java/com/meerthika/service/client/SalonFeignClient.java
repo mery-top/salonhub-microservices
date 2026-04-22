830093db8bd3a72018c0aedf18607a1226c814ee
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-22 05:56:28 +0530

    feat(review-feign): Add get salon by id feign client

--- review-service/src/main/java/com/meerthika/service/client/SalonFeignClient.java
+++ review-service/src/main/java/com/meerthika/service/client/SalonFeignClient.java
@@ -5,6 +5,7 @@ import com.meerthika.dto.SalonDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient("SALON-SERVICE")
@@ -13,6 +14,11 @@ public interface SalonFeignClient {
     @GetMapping("/api/salons/owner")
     public ResponseEntity<SalonDTO> getSalonsByOwnerId(
             @RequestHeader("Authorization") String jwt
+    ) throws Exception;
+
+    @GetMapping("api/salons/{salonId}")
+    public ResponseEntity<SalonDTO> getSalonsById(
+            @PathVariable Long salonId
     ) throws Exception;
 
 

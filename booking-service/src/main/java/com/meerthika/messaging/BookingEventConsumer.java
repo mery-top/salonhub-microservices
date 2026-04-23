df5098897db5b0911272348e40c89e6ce0c0d638
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-23 21:20:27 +0530

    feat(booking-rabbitmq): add BookingEventConsumer

---
+++ booking-service/src/main/java/com/meerthika/messaging/BookingEventConsumer.java
@@ -0,0 +1,20 @@
+package com.meerthika.messaging;
+
+
+import com.meerthika.modal.PaymentOrder;
+import com.meerthika.service.BookingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BookingEventConsumer {
+
+    private final BookingService bookingService;
+
+    @RabbitListener(queues = "booking-queue")
+    public void bookingUpdateListener(PaymentOrder paymentOrder) throws Exception {
+        bookingService.bookingSuccess(paymentOrder);
+    }
+}

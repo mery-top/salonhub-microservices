2b929142e95ae11ecd1eba4ff30541982ed34ce7
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-26 21:53:54 +0530

    feat(ai-dto): Create hair attribute dto

---
+++ ai-service/src/main/java/com/meerthika/dto/HairAttributeDTO.java
@@ -0,0 +1,11 @@
+package com.meerthika.dto;
+
+
+import lombok.Data;
+
+@Data
+public class HairAttributeDTO {
+    private String faceShape;
+    private String skinTone;
+    private String hairType;
+}

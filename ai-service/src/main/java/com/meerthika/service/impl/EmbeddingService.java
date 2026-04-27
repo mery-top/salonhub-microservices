65f0673285950dd7e1a29fc81334398ede4fe437
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-27 10:33:23 +0530

    refactor(ai-embed): Implement decode embedding for reducing size

--- ai-service/src/main/java/com/meerthika/service/impl/EmbeddingService.java
+++ ai-service/src/main/java/com/meerthika/service/impl/EmbeddingService.java
@@ -9,12 +9,15 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class EmbeddingService implements EmbeddingGenerator {
+
 
     private final WebClient webClient = WebClient.create("http://localhost:8001");
 
@@ -25,19 +28,34 @@ public class EmbeddingService implements EmbeddingGenerator {
         body.add("file", file.getResource());
 
         try {
-            Map response = webClient.post()
+            byte[] responseBytes = webClient.post()
                     .uri("/embed")
                     .contentType(MediaType.MULTIPART_FORM_DATA)
                     .bodyValue(body)
                     .retrieve()
-                    .bodyToMono(Map.class)
+                    .bodyToMono(byte[].class)
                     .block();
 
-            return (List<Float>) response.get("embedding");
+            return decodeEmbedding(responseBytes);
 
         } catch (Exception e) {
             throw new RuntimeException("Embedding service failed: " + e.getMessage());
         }
 
+    }
+
+    public List<Float> decodeEmbedding(byte[] bytes) {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+
+        FloatBuffer floatBuffer = byteBuffer.asFloatBuffer();
+
+        List<Float> embedding = new ArrayList<>(floatBuffer.remaining());
+
+        while (floatBuffer.hasRemaining()) {
+            embedding.add(floatBuffer.get());
+        }
+
+        return embedding;
     }
 }

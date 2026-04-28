ee0affba5cd2d738f246cfc9e2bb7c8ce24e1dc1
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-28 10:17:31 +0530

    build(ai-git): Remove unwanted files from tracking

--- ai-service/src/main/java/com/meerthika/service/EmbeddingGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.meerthika.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
-
-public interface EmbeddingGenerator {
-    public List<Float> generateEmbedding(MultipartFile file);
-}

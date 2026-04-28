4a1a5963c919c9bc1b6d95f9938a13c939265493
Meerthika <meerthikasrrajendrans@gmail.com>
2026-04-28 11:26:39 +0530

    feat(gateway-config): Add ai service to the gateway config

--- gateway-server/src/main/java/com/meerthika/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/meerthika/config/SecurityConfig.java
@@ -45,7 +45,8 @@ public class SecurityConfig {
                                 "/api/payments/**",
                                 "/api/service-offering/**",
                                 "/api/users/**",
-                                "/api/reviews/**"
+                                "/api/reviews/**",
+                                "/api/hair/**"
                                 )
                         .hasAnyRole("CUSTOMER", "SALON_OWNER", "ADMIN")
 

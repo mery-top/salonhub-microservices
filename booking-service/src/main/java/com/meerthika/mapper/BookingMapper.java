d1e465ddda257f2ee95d24faad23a03533b4fe2e
Meerthika <meerthikasrrajendrans@gmail.com>
2026-05-07 20:26:53 +0530

    refactor(booking-mapper): Add mapper for services, user and salon

--- booking-service/src/main/java/com/meerthika/mapper/BookingMapper.java
+++ booking-service/src/main/java/com/meerthika/mapper/BookingMapper.java
@@ -1,12 +1,18 @@
 package com.meerthika.mapper;
 
 import com.meerthika.dto.BookingDTO;
+import com.meerthika.dto.SalonDTO;
+import com.meerthika.dto.ServiceDTO;
+import com.meerthika.dto.UserDTO;
 import com.meerthika.modal.Booking;
+
+import java.util.List;
+import java.util.Set;
 
 public class BookingMapper {
 
 
-    public static BookingDTO toDTO(Booking booking){
+    public static BookingDTO toDTO(Booking booking, Set<ServiceDTO> services, SalonDTO salon, UserDTO user){
         BookingDTO bookingDTO = new BookingDTO();
         bookingDTO.setId(booking.getId());
         bookingDTO.setCustomerId(booking.getCustomerId());
@@ -16,6 +22,9 @@ public class BookingMapper {
         bookingDTO.setSalonId(booking.getSalonId());
         bookingDTO.setServiceIds(booking.getServiceIds());
         bookingDTO.setTotalPrice(booking.getTotalPrice());
+        bookingDTO.setServices(services);
+        bookingDTO.setUser(user);
+        bookingDTO.setSalon(salon);
 
         return bookingDTO;
     }

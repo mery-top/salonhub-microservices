4b14d59dbcde21c55cf5d385d3f41343b7f96a79
Meerthika <meerthikasrrajendrans@gmail.com>
2026-05-08 07:25:34 +0530

    feat(notification-ws): Add configureMessageBroker function

--- notification-service/src/main/java/com/meerthika/configuration/WebsocketConfig.java
+++ notification-service/src/main/java/com/meerthika/configuration/WebsocketConfig.java
@@ -2,6 +2,7 @@ package com.meerthika.configuration;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -18,6 +19,12 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/notification", "/user", "/chat");
+        registry.setUserDestinationPrefix("/user");
+    }
 }
 
 

b83c68f409e1fdb0d40a2d356861d78eb1ee1b89
Meerthika <meerthikasrrajendrans@gmail.com>
2026-05-08 08:05:45 +0530

    feat(notification-impl): Add RealTimeCommunicationService implementation

---
+++ notification-service/src/main/java/com/meerthika/service/impl/RealTimeCommunicationService.java
@@ -0,0 +1,31 @@
+package com.meerthika.service.impl;
+
+
+import com.meerthika.payload.dto.NotificationDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RealTimeCommunicationService {
+
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+
+
+    public void sendNotification(NotificationDTO notification) {
+        simpMessagingTemplate.convertAndSend(
+                "/notification/user/"+notification.getUserId(),
+                notification
+        );
+        simpMessagingTemplate.convertAndSend(
+                "/notification/salon/"+notification.getSalonId(),
+                notification
+        );
+    }
+
+
+
+}
